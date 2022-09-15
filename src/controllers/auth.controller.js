d3d13bbe08f26a7ed2bfff5216ba2d46365fa791
jeyson ortiz <jeisonortis12@gmail.com>
2022-09-14 23:35:07 -0500

    iniciando el funcionamiento de los controllers

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+const login = async (req, res ) => {
+
+}
+
+const register = async (req, res) => {
+
+}
+
+module.exports = { login, register }
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,21 @@
+const deleteUser = async (req, res ) => {
+
+}
+
+const getAllUsers = async (req, res ) => {
+
+}
+
+const getUserId = async (req, res ) => {
+
+}
+
+const getUserEmail = async (req, res ) => {
+
+}
+
+const updateUser = async (req, res ) => {
+
+}
+
+module.exports = { deleteUser, getAllUsers, getUserId, getUserEmail, updateUser };
